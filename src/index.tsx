81c43f57bbba3dc8fd9edfc2acd79daba479b661
Roman <romto2016@gmail.com>
2024-07-06 10:28:40 -0500

    add App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// React Element
+// const h1 = React.createElement('h1', { id: 'title' }, 'Hello World')
+// const p = React.createElement('p', null, 'Blablabla')
+
+// const app = (
+//     <div>
+//         {h1}
+//         {p}
+//     </div>
+// )
+
+// const h1 = <h1 id="title">Hello World</h1>
+// const p = (
+//     <p>
+//         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi hic
+//         unde at vero error magnam, possimus illo quae perferendis. Possimus
+//         recusandae omnis saepe voluptas consequatur repudiandae repellendus, ex
+//         quod quas.
+//     </p>
+// )
+// const ul = (
+//     <ul>
+//         <li>list item 1</li>
+//         <li>list item 2</li>
+//         <li>list item 3</li>
+//     </ul>
+// )
+// const app = (
+//     <div>
+//         {h1}
+//         {p}
+//         {ul}
+//     </div>
+// )
+
+// function declaration
+// function Title() {
+//     return <h1>Hello Title Component</h1>
+// }
+// function Content() {
+//     return (
+//         <p>
+//             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+//             corporis eveniet, nam nulla deserunt atque laboriosam ipsam possimus
+//             vitae odit dolorem doloremque nemo provident numquam quia recusandae
+//             officia sit repellat.
+//         </p>
+//     )
+// }
+
+// arrow function
+const Title = () => <h1>Hello Title Component</h1>
+const App = () => {
+    return (
+        <div>
+            <Title />
+            <Content />
+        </div>
+    )
+}
+
+const Content = () => (
+    <div>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            corporis eveniet, nam nulla deserunt atque laboriosam ipsam possimus
+            vitae odit dolorem doloremque nemo provident numquam quia recusandae
+            officia sit repellat.
+        </p>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi modi
+            assumenda pariatur perspiciatis rerum commodi vitae dolor, nihil
+            placeat sed est dolores aliquid in, facilis eligendi? At voluptates
+            rem modi?
+        </p>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)

77691b7788982f70486b9da9b009727d15fa0482
Roman <romto2016@gmail.com>
2024-07-13 10:58:12 -0500

    clean in index.tsx all component

--- src/index.tsx
+++ src/index.tsx
@@ -4,34 +4,10 @@ import Header from './header'
 import Content from './Content'
 
 const App = () => {
-    return (
+    return 
         <>
-            <Header
-                order={1}
-                text="Title"
-                leftpart="Hello"
-                rightpart="Test"
-                background="aqua"
-                color="blue"
-            />
-            <Header
-                text="App"
-                leftpart="Hi"
-                rightpart="Bla-Bla"
-                background="green"
-                color="purple"
-            />
-            <Header text="React" leftpart="Bye-Bye" rightpart="Rainbow" />
-
-            <Content title="Content 1" text1="hello 1" text2="Hello 2" />
-
-            <Content
-                title="Content 2"
-                text1="hello 1"
-                text2="Hello 2 bla-bla-bla"
-            />
         </>
-    )
+    
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)

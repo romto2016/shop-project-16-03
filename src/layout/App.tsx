9fb236427fbdd2e0979d761bbd724e2c7a76b0c5
Roman <romto2016@gmail.com>
2024-07-13 12:01:51 -0500

    add cssbaseline

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,7 +1,10 @@
 import Header from './Header'
+import CssBaseline from '@mui/material/CssBaseline'
+
 const App = () => {
     return (
         <>
+            <CssBaseline />
             <Header />
         </>
     )

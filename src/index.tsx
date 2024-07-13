35b8746b44f3c4853875fed829e040c4241e08d9
Roman <romto2016@gmail.com>
2024-07-13 11:52:23 -0500

    MUI install and add all depend

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './header'
-import Content from './Content'
-
-const App = () => {
-    return 
-        <>
-        </>
-    
-}
+import App from './layout/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

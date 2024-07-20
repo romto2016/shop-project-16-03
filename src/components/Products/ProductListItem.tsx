813d3195c3e7e1c58e105874139c9fdd5d6905b3
Roman <romto2016@gmail.com>
2024-07-20 12:12:46 -0500

    add title in props

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,16 +1,19 @@
 import { Button, Card, CardContent } from '@mui/material'
-
-type Props = {}
-const ProductListItem = (props: Props) => {
+import './ProductListItem.css'
+type Props = {
+    title: string
+}
+const ProductListItem = ({title}: Props) => {
     return (
-        <Card>
+        <Card variant="outlined" className="product-list-item">
             <CardContent>
-                <h3>iPhone 15</h3>
-                <p>This is iPhone 15</p>
-                <div>Type: phone</div>
-                <div>Capacity: 64Gb</div>
-                <div>$ 800</div>
-                <Button>Add to Card</Button>
+                <h3 className="product-title">{title}</h3>
+                <p className="product-description">This is iPhone 15</p>
+                <div className="product-features">Type: phone</div>
+                <div className="product-price">Capacity: 64Gb</div>
+                <div className="btns-wrapper">
+                    <Button variant="outlined">Add to Card</Button>
+                </div>
             </CardContent>
         </Card>
     )

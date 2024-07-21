01ade2aeef76549e338b499e3442e5a71f60b9d9
Roman <romto2016@gmail.com>
2024-07-20 22:25:17 -0500

    add props des,cap,price

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -19,15 +19,30 @@ const ProductList = (props: Props) => {
 
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={6} lg={4}>
-                    <ProductListItem title="IPhone 15" />
+                    <ProductListItem
+                        title="IPhone 15"
+                        description="This is iPhone 15"
+                        capacity="Capacity: 254Gb"
+                        price="Price: $2000"
+                    />
                 </Grid>
 
                 <Grid item xs={12} sm={6} lg={4}>
-                    <ProductListItem title="IPhone 14" />
+                    <ProductListItem
+                        title="IPhone 14"
+                        description="This is iPhone 14"
+                        capacity="Capacity: 128Gb"
+                        price="Price: $1000"
+                    />
                 </Grid>
 
                 <Grid item xs={12} sm={6} lg={4}>
-                    <ProductListItem title="IPhone 13" />
+                    <ProductListItem
+                        title="IPhone 13"
+                        description="This is iPhone 13"
+                        capacity="Capacity: 64Gb"
+                        price="Price: $500"
+                    />
                 </Grid>
             </Grid>
         </>
